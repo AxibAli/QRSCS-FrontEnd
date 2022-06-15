611c8baddfe66c2c956382ae0decc2c39453c4c7
munzah-shah <munzah.shah00@gmail.com>
2022-06-15 20:09:41 +0500

    Added successfully!

---
+++ Visual Studio Folder/QRSCS_MS/Controllers/EditorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QRSCS_MS.Controllers
+{
+    public class EditorController : Controller
+    {
+        // GET: Editor
+        public ActionResult IEP()
+        {
+            return View();
+        }
+    }
+}
---
+++ Visual Studio Folder/QRSCS_MS/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QRSCS_MS.Models;
+
+namespace QRSCS_MS.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel lgm)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Dashboard", "Dashboard");
+            }
+            else
+            {
+                return View();
+            }
+            
+
+            
+        }
+    }
+}
