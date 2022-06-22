f13872dc9ee74e3e07ee69ed8f0fa0945213ac78
munzah-shah <munzah.shah00@gmail.com>
2022-06-22 17:52:50 +0500

    Lost and Found Updated!

---
+++ Lost_And_Found/Controllers/ViewAdminsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lost_And_Found.Controllers
+{
+    public class ViewAdminsController : Controller
+    {
+        // GET: ViewAdmins
+        public ActionResult ViewAdmins()
+        {
+            return View();
+        }
+    }
+}
